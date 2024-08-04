45a540c64756ea2efb306401d527908719fed609
DuongM3Pro <nguyenkhuongduong1996@gmail.com>
2024-08-04 14:26:52 +0700

    [init] - Init project

---
+++ WalkthroughMorphingExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkthroughMorphingExample
+//
+//  Created by Khương Dương Nguyễn on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkthroughMorphingExample/WalkthroughMorphingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughMorphingExampleApp.swift
+//  WalkthroughMorphingExample
+//
+//  Created by Khương Dương Nguyễn on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WalkthroughMorphingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
